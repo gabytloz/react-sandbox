3403644b92a265fc9808e75bab574be21bda2de8
Enrique Vidal <enrique@cloverinteractive.com>
2016-08-12 11:40:02 -0700

    Handle routes smooth scrolling based on pathname

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
-import { Route, Router, IndexRoute, browserHistory } from "react-router";
+import { Route, Router, IndexRoute, browserHistory, applyRouterMiddleware } from "react-router";
+import { useScroll } from 'react-router-scroll';
+import animatedScrollTo from "animated-scrollto";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/application.scss";
 
@@ -11,14 +14,23 @@ import Services   from "./components/section/services";
 import Portfolio  from "./components/section/portfolio";
 import Contact    from "./components/section/contact";
 
-const router = <Router history={ browserHistory } >
+const shouldUpdateScroll = (prevRouterProps, { location }) => {
+  const elementId = location.pathname.replace( /^\//, '');
+  const element = document.getElementById( elementId );
 
+  if ( element  === null ) return true;
+
+  animatedScrollTo( document.body, element.offsetTop, 350 );
+  return false;
+}
+
+const router = <Router history={ browserHistory } render={ applyRouterMiddleware( useScroll( shouldUpdateScroll ) ) } >
   <Route path="/" component={ App }>
     <IndexRoute component={ Home } />
-    <Route path="about" component={About} />
-    <Route path="services" component={Services} />
-    <Route path="portfolio" component={Portfolio} />
-    <Route path="contact" component={Contact} />
+    <Route path="about" component={Home} />
+    <Route path="services" component={Home} />
+    <Route path="portfolio" component={Home} />
+    <Route path="contact" component={Home} />
   </Route>
 </Router>
 
